b279842fb494e210636838f874521804734bb7a9
Ralph Sommer <ralphsommer@gmail.com>
2024-01-22 13:49:27 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div>Hello, World!</div>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Pizza from "./Pizza";
+function Menu() {
+  return (
+    <>
+      <h1>Menu</h1>
+      <Pizza />
+    </>
+  );
+}
+
+export default Menu;
